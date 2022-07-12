e71e5e0822d5dae882ed1afb4dabcffe9c926312
Théo COURTIN <34097019+TheoCourtin@users.noreply.github.com>
2022-07-12 16:26:12 +0200

    Update modal.js

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -12,10 +12,15 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelectorAll(".close");
+const checkbox1 = document.querySelectorAll(".location1");
+const btn = document.querySelectorAll('.btn-submit');
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+btn.forEach((btn) => btn.addEventListener("click", checkAgree));
 
 // launch modal form
 function launchModal() {
@@ -26,16 +31,61 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+function checkAgree
+{
+  var submit = document.forms["formData"]['input[name=["checkbox2-label"]:checked'];
+  if (submit.checked) {
+    alert ('hi');
+} else {
+    alert ('bye');
+}
+}
+
+
+
+
+
 function acceptValue() 
 {
-  // var name = document.forms["formData"]["first"];
-  var prenom = document.forms["first"];
+  var prenom = document.forms["formData"]["first"].value;
+  var nom = document.forms["formData"]["last"].value;
+  var email = document.forms["formData"]["email"].value;
+  var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var birthdate = document.forms["formData"]["birthdate"].value;
+  var checkbox = documen.forms["formData"]['input[name="location"]:checked'];
+  var checkbox2 = documen.forms["formData"]['input[name="checkbox2"]:checked'];
+  
+  
+  if (prenom == "" || prenom == null)                                  
+  { 
+    alert("Ce champ ne doit pas être vide.");
+    return false;
 
-  if (prenom.value == " ")
-  {
-    alert("Ce champ ne doit pas être vide");
-    formData["first"].focus();  
-    
-    return false;   
   }
+
+  if (nom == "")                                  
+  { 
+    alert("Ce champ ne doit être pas vide.");
+  }
+
+  if (email == "" || !regex.test(email)) {
+    alert("Ce champ ne respecte pas les conditions.");
+    return false;
+  }
+
+  if (birthdate == "" || birthdate == "jj/mm/aa")
+  {
+    alert("Ce champ ne remplis pas les conditions.");
+  }
+
+  if( checkbox == false)
+  {
+    alert("Veuillez cocher une ville.");
+  }
+
+  if( checkbox2 == false)
+  {
+    alert("Vous n'avez pas accepté les conditions d'utilisations");
+  } 
+    
 }
