fda1a42afb61d11ef26be252a88476e609f9aae0
Théo COURTIN <34097019+TheoCourtin@users.noreply.github.com>
2022-08-03 15:47:22 +0200

    Update modal.js

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -11,183 +11,153 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelector(".close");
-
+const modalCross = document.getElementsByClassName("close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
+//  #1 TODO : close modal
+
+// Close modal form
 function closeModal() {
   modalbg.style.display = "none";
+};
+
+// Close modal event
+modalCross[0].addEventListener ("click", closeModal);
+
+
+// #2 Implement form entries
+
+// form must be valide when user click on "submit"
+const form = document.getElementById ('#form');
+const firstName = document.getElementById ('"first');
+const lastName = document.getElementById ('#last');
+const eMail = document.getElementById ('#email');
+const birthDate = document.getElementById ('#birthdate');
+const quantityTournament = document.getElementById ('#quantity');
+const loc1 = document.getElementById ('#location1');
+const loc2 = document.getElementById ('#location2');
+const loc3 = document.getElementById ('#location3');
+const loc4 = document.getElementById ('#location4');
+const loc5 = document.getElementById ('#location5');
+const loc6 = document.getElementById ('#location6');
+
+const dateFormat = /^\d{2}[./-]\d{2}[./-]\d{4}$/;
+const numbers = /^[0-9]+$/;
+
+
+// form.addEventListener('submit', (e) => {
+  // e.preventDefault();
+// })
+
+function validate () {
+	form.addEventListener('submit', (e) => {
+	e.preventDefault();
+})
+  if (firstName.value === '' && firstName.lenght > 1) {
+    alert ("Veuillez remplir le champ Prénom");
+    return false;
+  } 
+  else if (lastName.value === '' && lastName.lenght > 1) { 
+    alert ("Veuillez remplir le champ Nom");
+    return false;
+  } 
+  else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(eMail.value)) { 
+    alert ("Veuillez remplir le champ email");
+    return false;
+  }
+  // else if (!birthDate.value.match(dateFormat)) { 
+  //   alert ("Veuillez remplir votre date d'anniversaire");
+  //   return false;
+  // }
+  else if (!quantityTournament.value.match(numbers)) { 
+    alert ("Veuillez indiquer le nombre de tournois");
+    return false;
+  }
+  else if (!quantityTournament.value.match(numbers)) { 
+    alert ("Veuillez indiquer le nombre de tournois");
+    return false;
+  }
+  else if (!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) { 
+    alert ("Veuillez choisir une ville");
+    return false;
+  }
+
 }
 
-  // Inputs Value
-const first = document.getElementById("first");
-const last = document.getElementById("last");
-const email = document.getElementById("email");
-const birthdate = document.getElementById("birthdate");
-const quantity = document.getElementById("quantity");
-const location1 = document.getElementById("location1");
-const location2 = document.getElementById("location2");
-const location3 = document.getElementById("location3");
-const location4 = document.getElementById("location4");
-const location5 = document.getElementById("location5");
-const location6 = document.getElementById("location6");
-const checkbox1 = document.getElementById("checkbox1");
+//V2
 
-// // Regex
-const nameRegex = /^[a-z ,.'-][a-z ,.'-]+$/i;
-const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-const quantityRegex = /^[0-9]+/;
-const birthdateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+// const prenom = document.getElementById("first");
+// const nom =  document.getElementById("last");
+// const email =  document.getElementById("email");
+// const naissance = document.getElementById("birthdate");
+// const nombreTournois = document.getElementById("quantity");
+// const location1 = document.getElementById("location1");
+// const location2 =  document.getElementById("location2");
+// const location3 =  document.getElementById("location3");
+// const location4 =  document.getElementById("location4");
+// const location5 =  document.getElementById("location5");
+// const location6 =  document.getElementById("location6");
+// const checkbox1 = document.getElementById("checkbox1");
+// const checkbox2 = document.getElementById("checkbox2");
 
 
-// div where to insert error text
-const errorText = [
-  "textErrorFirst",
-  "textErrorLast",
-  "textErrorEmail",
-  "textErrorBirthdate",
-  "textErrorQuantity",
-  "textErrorLocation",
-  "textErrorCheckbox",
-];
+// var mailCaractere = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,})
+// )$/;
 
-// all the error texts
-const errorMsg = [
-  "Veuillez entrer 2 caractères ou plus pour le champ du prénom",
-  "Veuillez entrer 2 caractères ou plus pour le champ du nom",
-  "Merci de renseigner une adresse email valide",
-  "Vous devez entrer votre date de naissance",
-  "Merci d'indiquer votre nombre de games",
-  "Vous devez choisir une ville",
-  "Vous devez vérifier que vous acceptez les termes et conditions",
-];
+// function validate () {
 
-// function to validate First name, Last name, Email, Birthdate, Quantity (tournament)
-const validInput = (input, regex, errorTextId, errorMsg) => {
-  let inputValue = input.value;
-  if (regex.test(inputValue)) {
-    document.getElementById(errorTextId).innerText = "";
-    return true;
-  } else {
-    document.getElementById(errorTextId).innerHTML = errorText;
-    return false;
-  }
-};
+  // if (prenom.value === '' || prenom.value.length< 2)
 
-// function radio
-const validateRadio = (event) => {
-  let checkedLocation1 = location1.checked;
-  let checkedLocation2 = location2.checked;
-  let checkedLocation3 = location3.checked;
-  let checkedLocation4 = location4.checked;
-  let checkedLocation5 = location5.checked;
-  let checkedLocation6 = location6.checked;
-  if (
-    checkedLocation1 == false &&
-    checkedLocation2 == false &&
-    checkedLocation3 == false &&
-    checkedLocation4 == false &&
-    checkedLocation5 == false &&
-    checkedLocation6 == false
-  ) {
-    document.getElementById(errorText[5]).innerHTML = errorMsg[5];
-    return false;
-  } else {
-    document.getElementById(errorText[5]).innerHTML = "";
-    return true;
-  }
-};
+  // {
+    // alert ("Remplir votre prénom avec 2 lettres minimums");  
+    // event.preventDefault();
+  // }
 
-// function checkbox (CGU)
-const validateCheckbox = (event) => {
-  if (!conditionsCheckBox1.checked) {
-    document.getElementById(errorText[6]).innerHTML = errorMsg[6];
-    return false;
-  } else {
-    document.getElementById(errorText[6]).innerHTML = "";
-    return true;
-  }
-};
+  // if (nom.value === '' || nom.value.length < 2)
+  // {
+    // alert ("Remplir votre nom avec 2 lettres minimums");  
+    // event.preventDefault();
+  // }
+  // if (naissance.value ===''){
+    // alert("entrez une date de naissance");
+    // event.preventDefault();
+  // }
 
-// FONCTION DE VALIDATION
-// First name
-const validate = () => {
-  const isFirstNameValid = validInput(
-    first,
-    nameRegex,
-    errorText[0],
-    errorMsg[0]
-  );
-  // Last name
-  const isLastNameValid = validInput(
-    last,
-    nameRegex,
-    errorText[1],
-    errorMsg[1]
-  );
-  // Email
-  const isEmailValid = validInput(
-    email,
-    emailRegex,
-    errorText[2],
-    errorMsg[2]
-  );
-  // Birthdate
-  const isBirthdateValid = validInput(
-    birthdate,
-    quantityRegex,
-    errorText[3],
-    errorMsg[3]
-  );
-  // Quantity (tournament)
-  const isQuantityValid = validInput(
-    quantity,
-    quantityRegex,
-    errorText[4],
-    errorMsg[4]
-  );
-  // Radio
-  const isRadioValid = validateRadio();
-  // Checkbox (CGU)
-  const isCheckboxValid = validateCheckbox();
+  // if (nombreTournois.value === ''){
+    // alert ("Indiquez le nombre de tournois GameOn");
+    // event.preventDefault();
+  // }
 
-  return (
-    isFirstNameValid &&
-    isLastNameValid &&
-    isEmailValid &&
-    isBirthdateValid &&
-    isQuantityValid &&
-    isRadioValid &&
-    isCheckboxValid
-  );
-};
+  // if(mailCaractere.test(document.getElementById("email").value)){
+    // alert(" OK");
+  // }
+  // else{
+    // alert ("Entrez une adresse mail correct ");  
+    // event.preventDefault();
+  // }
 
-// form sending
-document.getElementById("inscription").addEventListener("submit", function (event) {
-    event.preventDefault(); // on le met si erreur afin de ne pas envoyé le formulaire
+  // if ((location1.checked)|| (location2.checked) || (location3.checked)
+    // ||(location4.checked) ||(location5.checked) ||(location6.checked)) {
+    // alert("bouton selectionné");
+  // }
+  // else{
+    // alert ("Selectionner un bouton"); 
+    // event.preventDefault();
+  // }
 
-    if (validate(event)) {
-      const messageValidation = document.getElementById("submitMessage");
-      messageValidation.style.display = "block";
-      const form = document.getElementById("inscription");
-      form.style.display = "none";
-    }
-  });
+  // if(checkbox1.checked){
+    // alert("Condition selectionné");
+  // }
+  // else{
+    // alert("Veuillez accepter les condtions d'utilisation");
+    // event.preventDefault();
+  // }
 
-// close button after validation
-const btnValidation = document.querySelector("btn-validation");
-if (btnValidation) 
-{
-	btnValidation.addEventListener("click", close);
-}
-
-
+// }
