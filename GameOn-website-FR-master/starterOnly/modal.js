da3291319c6a53b9cdb2c3da28425a18a2089029
Théo COURTIN <34097019+TheoCourtin@users.noreply.github.com>
2022-08-22 07:21:08 +0200

    Add files via upload

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -35,18 +35,19 @@ modalCross[0].addEventListener ("click", closeModal);
 // #2 Implement form entries
 
 // form must be valide when user click on "submit"
-const form = document.getElementById ('#form');
-const firstName = document.getElementById ('"first');
-const lastName = document.getElementById ('#last');
-const eMail = document.getElementById ('#email');
-const birthDate = document.getElementById ('#birthdate');
-const quantityTournament = document.getElementById ('#quantity');
-const loc1 = document.getElementById ('#location1');
-const loc2 = document.getElementById ('#location2');
-const loc3 = document.getElementById ('#location3');
-const loc4 = document.getElementById ('#location4');
-const loc5 = document.getElementById ('#location5');
-const loc6 = document.getElementById ('#location6');
+const form = document.getElementById ('form');
+const firstName = document.getElementById ('first');
+const lastName = document.querySelector ('#last');
+const eMail = document.getElementById ('email');
+const birthDate = document.getElementById ('birthdate');
+const quantityTournament = document.getElementById ('quantity');
+const loc1 = document.getElementById ('location1');
+const loc2 = document.getElementById ('location2');
+const loc3 = document.getElementById ('location3');
+const loc4 = document.getElementById ('location4');
+const loc5 = document.getElementById ('location5');
+const loc6 = document.getElementById ('location6');
+const checkbox1 = document.getElementById("checkbox1");
 
 const dateFormat = /^\d{2}[./-]\d{2}[./-]\d{4}$/;
 const numbers = /^[0-9]+$/;
@@ -56,40 +57,39 @@ const numbers = /^[0-9]+$/;
   // e.preventDefault();
 // })
 
-function validate () {
+
 	form.addEventListener('submit', (e) => {
 	e.preventDefault();
-})
-  if (firstName.value === '' && firstName.lenght > 1) {
-    alert ("Veuillez remplir le champ Prénom");
+
+  console.log(lastName.value);
+  if (firstName.value.length < 2) {
+    document.querySelector(".msg_firstname").textContent = "Veuillez remplir le champ Prénom";
     return false;
   } 
-  else if (lastName.value === '' && lastName.lenght > 1) { 
+   if (lastName.value.length < 2) { 
     alert ("Veuillez remplir le champ Nom");
     return false;
   } 
-  else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(eMail.value)) { 
+   if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(eMail.value)) { 
     alert ("Veuillez remplir le champ email");
     return false;
-  }
-  // else if (!birthDate.value.match(dateFormat)) { 
-  //   alert ("Veuillez remplir votre date d'anniversaire");
-  //   return false;
-  // }
-  else if (!quantityTournament.value.match(numbers)) { 
+  }  
+   if (!quantityTournament.value.match(numbers)) { 
     alert ("Veuillez indiquer le nombre de tournois");
     return false;
-  }
-  else if (!quantityTournament.value.match(numbers)) { 
-    alert ("Veuillez indiquer le nombre de tournois");
-    return false;
-  }
-  else if (!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) { 
+  }  
+   if (!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) { 
     alert ("Veuillez choisir une ville");
     return false;
   }
 
-}
+  if (!checkbox1.checked)
+  {
+    alert ("Veuillez cocher cette case");
+    return false;
+  }
+})
+
 
 //V2
 
